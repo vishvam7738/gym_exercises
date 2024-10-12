9bcca685d747b14fd2cabdcce5136de5684dc252
vishvam <vishvamaggarwal@gmail.com>
2024-10-12 12:12:22 +0530

    Developed Horizontal Scroll Bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises,bodyPart,setBodyPart}) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
@@ -77,7 +77,9 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{position:'relative', width:'100%', p:'20px'}}>
-          <HorizontalScrollbar data={bodyParts}/>
+          <HorizontalScrollbar data={bodyParts}
+          bodyPart={bodyPart} setBodyPart={setBodyPart}
+          />
       </Box>
     </Stack>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,20 @@ import SearchExercises from '../components/SearchExercises'
 import HeroBanner from '../components/HeroBanner'
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises/>
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart} />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   )
 }
